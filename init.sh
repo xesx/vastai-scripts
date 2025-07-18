1d61c6f3e2623a52b506c7dd46562100e168d293
Alexey Alekhin <alekhin@sportxline.com>
2025-07-18 11:49:04 +0300

    init.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Получаем путь к самому исполняемому скрипту
+SCRIPT_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+TARGET_SCRIPT="$SCRIPT_DIR/import-from-ydisk.sh"
+ALIAS_NAME="yaimport"
+
+# Определяем файл конфигурации shell'а
+if [[ $SHELL == */zsh ]]; then
+    SHELL_RC="$HOME/.zshrc"
+else
+    SHELL_RC="$HOME/.bashrc"
+fi
+
+echo TARGET_SCRIPT: "$TARGET_SCRIPT"
+
+# Добавляем алиас, если его ещё нет
+if grep -q "alias $ALIAS_NAME=" "$SHELL_RC"; then
+    echo "Алиас '$ALIAS_NAME' уже существует в $SHELL_RC"
+else
+    echo "alias $ALIAS_NAME=\"$TARGET_SCRIPT\"" >> "$SHELL_RC"
+    echo "Алиас '$ALIAS_NAME' добавлен в $SHELL_RC"
+    echo "Чтобы применить изменения, запусти: source $SHELL_RC"
+fi
--- start-flux.sh
+++ start-flux.sh
@@ -19,8 +19,9 @@ PIP_PACKAGES=(
 
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
-    "https://github.com/cubiq/ComfyUI_essentials"
     "https://github.com/crystian/ComfyUI-Crystools"
+    "https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/twri/sdxl_prompt_styler"
 )
 
 # Make rclone config
