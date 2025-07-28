72238f78f977d034aacf6213ea393a2bb683a26a
Alexey Alekhin <alekhin@sportxline.com>
2025-07-28 16:26:06 +0300

    some imprvs

--- import-for-schema/flux-outpaint-01.sh
+++ import-for-schema/flux-outpaint-01.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-rclone copy -P ydisk:shared/comfyui/models/diffusion_models/flux1-fill-dev-FP8.safetensors /workspace/ComfyUI/models/diffusion_models/
-rclone copy -P ydisk:shared/comfyui/models/clip/clip_l.safetensors /workspace/ComfyUI/models/clip/
-rclone copy -P ydisk:shared/comfyui/models/clip/t5-v1_1-xxl-encoder-Q4_K_S.gguf /workspace/ComfyUI/models/clip/
+# Массив файлов для копирования
+FILES=(
+    "diffusion_models/flux1-fill-dev-FP8.safetensors"
+    "clip/clip_l.safetensors"
+    "clip/t5-v1_1-xxl-encoder-Q4_K_S.gguf"
+)
+
+# Копируем каждый файл
+for file in "${FILES[@]}"; do
+    echo "Копирую: $file"
+    rclone copy -P "$REMOTE_BASE/$file" "$LOCAL_BASE/$file"
+done
