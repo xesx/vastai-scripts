e7d48e90c3fefbc91afd4eb1225ea5cb307eafc5
Alexey Alekhin <alekhin@sportxline.com>
2025-07-28 16:36:50 +0300

    some imprvs

--- import-for-schema/flux-outpaint-01.sh
+++ import-for-schema/flux-outpaint-01.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Массив файлов для копирования
 FILES=(
-    "diffusion_models/flux1-fill-dev-FP8.safetensors"
     "clip/clip_l.safetensors"
     "clip/t5-v1_1-xxl-encoder-Q4_K_S.gguf"
+    "vae/Flux-vae.safetensors"
+    "diffusion_models/flux1-fill-dev-FP8.safetensors"
 )
 
 # Копируем каждый файл
