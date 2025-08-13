8bd0abc992f246d1ffe965acfe7c68e340f55ab7
Alexey Alekhin <alekhin@sportxline.com>
2025-08-13 22:04:34 +0300

    export outputs

--- export-output.sh
+++ export-output.sh
@@ -4,7 +4,7 @@
 DEST_FOLDER="output_$(date +'%Y-%m-%d-%H-%M')"
 
 # Копируем файлы на Яндекс.Диск
-rclone copy /workspace/ComfyUI/output yandex:output/"$DEST_FOLDER" --progress
+rclone copy /workspace/ComfyUI/output ydisk:output/"$DEST_FOLDER" --progress
 
 # Если копирование прошло успешно (код возврата 0), очищаем папку
 if [ $? -eq 0 ]; then
