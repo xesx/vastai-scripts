e5ad5477bbfb282c6208135cda915d597d112e96
Alexey Alekhin <alekhin@sportxline.com>
2025-08-28 18:29:33 +0300

    import secrets

--- import-secrets.sh
+++ import-secrets.sh
@@ -6,7 +6,7 @@ API_URL="https://us.infisical.com/api/v3/secrets/raw"
 response=$(curl -s -H "Authorization: Bearer $INFISICAL_TOKEN" "$API_URL")
 
 # Проходим по каждому секрету и экспортируем переменную
-echo "$response" | jq -r '.secrets[] | "export \(.secretKey)=\(.secretValue)"' >> ${WORKSPACE}/set_secrets.sh
+echo "$response" | jq -r '.secrets[] | "export \(.secretKey)=\(.secretValue)"' >> ${WORKSPACE}/onstart.sh
 
 # Опционально: показать, что переменные установились
 echo "Секреты загружены:"
